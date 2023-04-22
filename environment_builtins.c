7d4bc63493d4a9a61ba444b5518b56a6d177f9e2
Prof. Percival <110649262+Prof-Percival@users.noreply.github.com>
2023-04-22 22:49:34 +0200

    environment built ins

--- environment_builtins.c
+++ environment_builtins.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 
-int shell_environment(char **arguments, char __attribute__((__unused__)) **front);
+int shell_environmentironment(char **arguments, char __attribute__((__unused__)) **front);
 int shell_unset_environment(char **arguments, char __attribute__((__unused__)) **front);
 int shell_set_environment(char **arguments, char __attribute__((__unused__)) **front);
 
 /**
- * shell_environment - It prints the current environment.
+ * shell_environmentironment - It prints the current environment.
  * @arguments: An array of arguments passed to the shell.
  * @front: A double pointer to the beginning of arguments.
  *
@@ -15,17 +15,17 @@ int shell_set_environment(char **arguments, char __attribute__((__unused__)) **f
  * Description: It prints one variable per line in the
  *              format 'variable'='value'.
  */
-int shell_environment(char **arguments, char __attribute__((__unused__)) **front)
+int shell_environmentironment(char **arguments, char __attribute__((__unused__)) **front)
 {
 	int index;
 	char newline_character = '\n';
 
-	if (!environ)
+	if (!environment)
 		return (-1);
 
-	for (index = 0; environ[index]; index++)
+	for (index = 0; environment[index]; index++)
 	{
-		write(STDOUT_FILENO, environ[index], _strlen(environ[index]));
+		write(STDOUT_FILENO, environment[index], string_length(environment[index]));
 		write(STDOUT_FILENO, &newline_character, 1);
 	}
 
@@ -44,8 +44,8 @@ int shell_environment(char **arguments, char __attribute__((__unused__)) **front
  */
 int shell_unset_environment(char **arguments, char __attribute__((__unused__)) **front)
 {
-	size_t size;
-	char **environment_variable, **new_environment;
+	size_type size;
+	char **environment_variable, **new_environmentment;
 	int index, index2;
 
 	if (!arguments[0])
@@ -54,26 +54,26 @@ int shell_unset_environment(char **arguments, char __attribute__((__unused__)) *
 	if (!environment_variable)
 		return (0);
 
-	for (size = 0; environ[size]; size++)
+	for (size = 0; environment[size]; size++)
 		;
 
-	new_environment = malloc(sizeof(char *) * size);
-	if (!new_environment)
+	new_environmentment = malloc(sizeof(char *) * size);
+	if (!new_environmentment)
 		return (create_an_error(arguments, -1));
 
-	for (index = 0, index2 = 0; environ[index]; index++)
+	for (index = 0, index2 = 0; environment[index]; index++)
 	{
-		if (*environment_variable == environ[index])
+		if (*environment_variable == environment[index])
 		{
 			free(*environment_variable);
 			continue;
 		}
-		new_environment[index2] = environ[index];
+		new_environmentment[index2] = environment[index];
 		index2++;
 	}
-	free(environ);
-	environ = new_environment;
-	environ[size - 1] = NULL;
+	free(environment);
+	environment = new_environmentment;
+	environment[size - 1] = NULL;
 
 	return (0);
 }
@@ -90,19 +90,19 @@ int shell_unset_environment(char **arguments, char __attribute__((__unused__)) *
  */
 int shell_set_environment(char **arguments, char __attribute__((__unused__)) **front)
 {
-	char **environment_variable = NULL, **new_environment, *new_value;
-	size_t size;
+	char **environment_variable = NULL, **new_environmentment, *new_value;
+	size_type size;
 	int index;
 
 	if (!arguments[0] || !arguments[1])
 		return (create_an_error(arguments, -1));
 
-	new_value = malloc(_strlen(arguments[0]) + 1 + _strlen(arguments[1]) + 1);
+	new_value = malloc(string_length(arguments[0]) + 1 + string_length(arguments[1]) + 1);
 	if (!new_value)
 		return (create_an_error(arguments, -1));
-	_strcpy(new_value, arguments[0]);
-	_strcat(new_value, "=");
-	_strcat(new_value, arguments[1]);
+	_stringcopy(new_value, arguments[0]);
+	_stringcat(new_value, "=");
+	_stringcat(new_value, arguments[1]);
 
 	environment_variable = _get_environment(arguments[0]);
 	if (environment_variable)
@@ -111,23 +111,23 @@ int shell_set_environment(char **arguments, char __attribute__((__unused__)) **f
 		*environment_variable = new_value;
 		return (0);
 	}
-	for (size = 0; environ[size]; size++)
+	for (size = 0; environment[size]; size++)
 		;
 
-	new_environment = malloc(sizeof(char *) * (size + 2));
-	if (!new_environment)
+	new_environmentment = malloc(sizeof(char *) * (size + 2));
+	if (!new_environmentment)
 	{
 		free(new_value);
 		return (create_an_error(arguments, -1));
 	}
 
-	for (index = 0; environ[index]; index++)
-		new_environment[index] = environ[index];
+	for (index = 0; environment[index]; index++)
+		new_environmentment[index] = environment[index];
 
-	free(environ);
-	environ = new_environment;
-	environ[index] = new_value;
-	environ[index + 1] = NULL;
+	free(environment);
+	environment = new_environmentment;
+	environment[index] = new_value;
+	environment[index + 1] = NULL;
 
 	return (0);
 }
